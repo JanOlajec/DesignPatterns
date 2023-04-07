0f4b7dc050e24058ca695506fa9acb50558d7cd9
JanOlajec <54432321+JanOlajec@users.noreply.github.com>
2023-04-07 18:44:46 +0200

    Added comments.

--- StrategyDuck/Ducks/Duck.cs
+++ StrategyDuck/Ducks/Duck.cs
@@ -4,24 +4,39 @@ using StrategyDuck.Flying;
 
 namespace StrategyDuck
 {
+    /// <summary>
+    /// Super-class Duck for specific derived classes 
+    /// </summary>
     internal abstract class Duck
     {
         internal IQuack QuackingStrategy { get; set; }
         internal ISwim SwimingStrategy { get; set; }
         internal IFly FlyingStrategy { get; set; }
 
+        /// <summary>
+        /// Each derived class implement own Display() method which describe duck type.
+        /// </summary>
         public abstract void Display();
 
+        /// <summary>
+        /// Call delegate for Quack behavior (strategy)
+        /// </summary>
         public void Quack()
         {
             QuackingStrategy.Quack();
         }
 
+        /// <summary>
+        /// Call delegate for Swim behavior (strategy)
+        /// </summary>
         public void Swim()
         {
             SwimingStrategy.Swim();
         }
 
+        /// <summary>
+        /// Call delegate for Fly behavior (strategy)
+        /// </summary>
         public void Fly()
         {
             FlyingStrategy.Fly();
--- StrategyDuck/Program.cs
+++ StrategyDuck/Program.cs
@@ -7,8 +7,13 @@ namespace StrategyDuck
 {
     internal class Program
     {
+        /// <summary>
+        /// Duck simulator application :-) with approach of Strategy design pattern
+        /// </summary>
         static void Main()
-        {
+        {   
+            // Each duck-type have own approach how to do behavior (strategy): quack, swim, fly.
+            // See super-class Duck. 
             WildDuck wildDuck = new WildDuck();
             DomesticDuck domesticDuck = new DomesticDuck();
             WoodenDuck woodenDuck = new WoodenDuck();
@@ -32,6 +37,7 @@ namespace StrategyDuck
                 Console.WriteLine();
             }
 
+            // As duck realize behavior with delegate (over interface), it can be changed during runtime. 
             Console.WriteLine("Change behavior during runtime:");
             metalDuck.QuackingStrategy = new DoQuack();
             metalDuck.Display();
